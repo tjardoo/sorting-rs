65cef74e63eb6a1f426d7ba2148252c6d635f0dc
tjardoo <info@tjardo.com>
2023-02-18 20:23:48 +0100

    Initial sorting setup

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+pub trait Sorter {
+    fn sort<T>(slice: &mut [T]) where T: Ord;
+}
+
+pub fn sort<T, S>(slice: &mut [T])
+    where
+        T: Ord,
+        S: Sorter,
+{
+    S::sort(slice)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Sorter, sort};
+
+    struct StdSorter;
+
+    impl Sorter for StdSorter {
+        fn sort<T>(slice: &mut [T]) where T: Ord {
+            slice.sort()
+        }
+    }
+
+    #[test]
+    fn std_sorter_works() {
+        let mut things = vec![4, 2, 3, 1];
+
+        sort::<_, StdSorter>(&mut things);
+
+        assert_eq!(things, &[1, 2, 3, 4]);
+    }
+}
